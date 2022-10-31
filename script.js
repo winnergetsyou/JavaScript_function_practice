d7e959793b43ddc9647fa945fe3d78f67d5c9a11
winnergetsyou <97407880+winnergetsyou@users.noreply.github.com>
2022-10-30 22:08:06 -0400

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+
+ function changesInnerElement()
+ {
+  document.getElementById("first_paragraph").innerText = "You have sucessfully read the information. document.getElementById functions has been used";
+ }
